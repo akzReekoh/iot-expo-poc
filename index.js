8abb5021d8954aeefd23b44aa94654f0fdc7431d
akzReekoh <achilles.dinglasan@p3ople4u.com>
2016-02-16 18:26:20 +0800

    REEKOH-640
    -Minor enhancements

--- index.js
+++ index.js
@@ -30,10 +30,20 @@ io.on('connection', function(socket){
                 clInstance.grab(function(err, image){
                     if(!err){
                         fs.writeFile('./stream/image.jpg', image, 'binary', function(){
-                            mqttClient.publish('reekoh/data', JSON.stringify({image: new Buffer(image).toString('base64')}));
-                            console.log('sent');
-                            motion = cp.spawn('motion');
-                            setTimeout(function(){socket.emit('refresh');}, 800);
+                            fs.readFile('./stream/image.jpg', function(err, data) {
+                                if(err) {
+                                    console.error(err);
+                                } else {
+                                    var image = new Buffer(data).toString('base64');
+                                    mqttClient = mqtt.connect('mqtt://iotexpo.reekoh.com:19001', {clientId: 'raspi1'});
+                                    mqttClient.publish('reekoh/data', JSON.stringify({image: image}), function(){
+                                        mqttClient.end();
+                                        console.log('sent');
+                                        motion = cp.spawn('motion');
+                                        setTimeout(function(){socket.emit('refresh');}, 900);
+                                    });
+                                }
+                            });
                         });
                     }
                     else
@@ -49,5 +59,4 @@ io.on('connection', function(socket){
 http.listen(3000, function() {
     console.log('listening on *:3000');
     motion = cp.spawn('motion');
-    mqttClient = mqtt.connect('mqtt://iotexpo.reekoh.com:19001', {clientId: 'raspi1'});
 });
